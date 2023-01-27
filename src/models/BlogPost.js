e5418fde791f771b89fd9118c691f7f3d3b99f8a
igopereira1 <igopereira1@gmail.com>
2023-01-26 21:59:19 -0300

    feat (req10): criação do model BlogSpot

---
+++ src/models/BlogPost.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => {
+    const BlogPost = sequelize.define('BlogPost', {
+      id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, },
+      title: DataTypes.STRING,
+      content: DataTypes.DATE,
+      userId: { type: DataTypes.INTEGER, foreignKey: true, },
+      published: DataTypes.STRING,
+      updated: DataTypes.DATE,
+    },
+    {
+      timestamps: true,
+      tableName: 'blogPost',
+      underscored: true,
+    });
+    BlogPost.associate = (models) => {
+      BlogPost.belongsTo(models.User,
+        { foreignKey: 'id', as: 'userId' });
+    };
+
+    return BlogPost;
+  };
